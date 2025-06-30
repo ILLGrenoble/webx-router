61167baced0e1fdd2363a91ec58a28ef6ae67e76
Stuart Caunt <caunt@ill.fr>
2025-06-30 08:10:52 +0000

    Remove use of config file from CLI. Repeat pinging of WebX Engine from CLI and handle ctrl-c nicely.

--- src/app/cli.rs
+++ src/app/cli.rs
@@ -1,9 +1,21 @@
-use crate::common::{Settings, Result, RouterError};
+use crate::common::{Result, RouterError};
 use crate::router::SessionCreationReturnCodes;
+use crate::fs::chmod;
 
 use base64::engine::{general_purpose::STANDARD, Engine};
-use rand::{rng, Rng};
-use rand::distr::Alphanumeric;
+use rand::{
+    rng, 
+    Rng,
+    distr::Alphanumeric,
+};
+use std::{
+    fs::File,
+    io::Write,
+    thread,
+    time,
+    sync::{Mutex, Arc},
+};
+use std::time::{Duration, Instant};
 
 struct CommResponse {
     pub _publisher_port: u32,
@@ -33,26 +45,28 @@ impl Cli {
         }
     }
 
-    pub fn connect(&mut self, settings: &Settings) -> Result<()> {
-        info!("Connecting to WebX Router...");
+    pub fn connect(&mut self, connector_port: u32) -> Result<()> {
+        debug!("Connecting to WebX Router");
 
         // Create ZMQ context
         let context = zmq::Context::new();
 
-        let connector_port = settings.transport.ports.connector;
+        debug!("Connecting to connector socket on port {}", connector_port);
         let connector_socket = self.create_req_socket(&context, connector_port, None)?;
 
+        debug!("Sending comm request...");
         let response = self.send(&connector_socket, "comm")?;
-
         let comm_response = self.decode_comm_response(&response)?;
+        debug!("... received comm response {}", &response);
 
-        info!("Got session port {}", comm_response.session_port);
+        debug!("Got session socket port {}", comm_response.session_port);
 
         let session_socket = self.create_req_socket(&context, comm_response.session_port, Some(comm_response.public_key))?;
-
         let _ = self.session_socket.insert(SessionSocket { port: comm_response.session_port, socket: session_socket });
 
         self.disconnect_req_socket(&connector_socket, connector_port);
+
+        info!("Connected to WebX Router");
 
         Ok(())
     }
@@ -60,23 +74,76 @@ impl Cli {
     pub fn disconnect(&mut self) {
         if let Some(session_socket) = self.session_socket.as_ref() {
             self.disconnect_req_socket(&session_socket.socket, session_socket.port);
+            debug!("Disconnected from WebX Router");
         }
     }
 
     pub fn create(&self, width: u32, height: u32, keyboard_layout: &str) -> Result<CreationResponse> {
         let session_socket = self.session_socket.as_ref().ok_or_else(|| RouterError::SystemError(format!("Session Socket is unavailable")))?;
 
-        // Create credentials file in the user's home directory (ro by the user)
+        info!("Creating WebX Engine Session command with resolution {} x {} and keyboard layout {}", width, height, keyboard_layout);
 
-        // create a random password
+        // Create credentials file in /tmp (ro by the user)
+        let credentials_path = format!("/tmp/{}", self.create_random_string(8));
+        let mut file = File::create(&credentials_path)?;
+        chmod(&credentials_path, 0o600)?;
 
-        // Write the password to the file
+        // create and write a random password
+        let password = self.create_random_string(32);
+        file.write_all(password.as_bytes())?;
 
-        let create_request = format!("create,{},{},{},{},{}", self.encode_base64("/tmp/test"), self.encode_base64("password"), width, height, keyboard_layout);
+        debug!("Credentials written to {}", credentials_path);
 
+        debug!("Sending creation request to WebX Router...");
+        let create_request = format!("create,{},{},{},{},{}", self.encode_base64(&credentials_path), self.encode_base64(&password), width, height, keyboard_layout);
         let response = self.send(&session_socket.socket, &create_request)?;
 
+        debug!("... received response {}", response);
+
+        std::fs::remove_file(&credentials_path)?;
+
         self.decode_create_response(&response)
+    }
+
+    pub fn wait_for_interrupt(&self, session_id: &str) -> Result<()> {
+        let session_socket = self.session_socket.as_ref().ok_or_else(|| RouterError::SystemError(format!("Session Socket is unavailable")))?;
+
+        let running_mutex = Arc::new(Mutex::new(true));
+        let mut is_running = true;
+
+        let running_handler = Arc::clone(&running_mutex);
+        ctrlc::set_handler(move || {
+            debug!("CTRL-C received");
+            if let Ok(mut running) = running_handler.lock() {
+               *running = false;
+            }
+
+        }).expect("Error setting Ctrl-C handler");
+
+        let mut last_ping = Instant::now();
+        let ping_request = format!("ping,{}", session_id);
+        while is_running {
+            thread::sleep(time::Duration::from_millis(100));
+
+            if last_ping.elapsed() >= Duration::from_secs(5) {
+                debug!("Sending ping request to WebX Router...");
+                let ping_response = self.send(&session_socket.socket, &ping_request)?;
+                debug!("... received response {}", ping_response);
+                last_ping = Instant::now();
+
+                if !self.decode_ping_response(&ping_response) {
+                    return Err(RouterError::EngineSessionError(format!("Failed to ping engine")));
+                }
+            }
+
+            if let Ok(running) = running_mutex.lock() {
+                is_running = *running;
+            }
+        }
+
+        info!("Finished nicely");
+
+        Ok(())
     }
 
     fn decode_comm_response(&self, response: &str) -> Result<CommResponse> {
@@ -107,6 +174,12 @@ impl Cli {
         let code = SessionCreationReturnCodes::try_from(response_code_num)?;
 
         Ok( CreationResponse { code, message })
+    }
+
+    fn decode_ping_response(&self, response: &str) -> bool {
+        let response_parts = response.split(',').collect::<Vec<&str>>();
+
+        response_parts[0] == "pong"
     }
 
     fn send(&self, socket: &zmq::Socket, request: &str) -> Result<String> {
--- src/bin/cli/main.rs
+++ src/bin/cli/main.rs
@@ -2,7 +2,6 @@
 extern crate log;
 
 use webx_router::{
-    common::{Settings},
     app::Cli,
     router::SessionCreationReturnCodes,
 };
@@ -42,9 +41,13 @@ enum Command {
 #[derive(StructOpt, Debug)]
 #[structopt(name = "webx-cli-options")]
 struct Opt {
-    /// Path to the configuration file.
-    #[structopt(short, long, default_value = "")]
-    config: String,
+    /// Port to connect to
+    #[structopt(short, long, default_value = "5555")]
+    port: u32,
+
+    /// Produces verbose logging
+    #[structopt(short, long)]
+    verbose: bool,
 
     #[structopt(subcommand)]
     command: Command,
@@ -57,29 +60,31 @@ fn main() {
     // Parse command-line arguments.
     let opt = Opt::from_args();
 
-    // Load application settings from the specified configuration file.
-    let settings = Settings::new(&opt.config).expect("Loaded settings");
-
     // Initialize logging based on the settings.
-    if let Err(error) = setup_logging(&settings) {
+    if let Err(error) = setup_logging(opt.verbose) {
         error!("Failed to initialize logging: {}", error);
         process::exit(1);
     }
 
     let mut cli = Cli::new();
-    if let Err(error) = cli.connect(&settings) {
+    if let Err(error) = cli.connect(opt.port) {
         error!("Failed to connect: {}", error);
         std::process::exit(1);
     }
-
+    
     match opt.command {
         Command::Create {daemon, width, height, keyboard_layout} => {
-            info!("Got create command with daemon = {}", daemon);
             match cli.create(width, height, &keyboard_layout) {
                 Ok(response) => {
                     match response.code {
                         SessionCreationReturnCodes::Success => {
-                            info!("WebX Engine running with session Id {}", response.message);
+                            let session_id = response.message;
+                            info!("WebX Engine running with session Id {}", &session_id);
+                            if !daemon {
+                                if let Err(error) = cli.wait_for_interrupt(&session_id) {
+                                    error!("Failed to wait for WebX Engine process: {}", error);
+                                }
+                            }
                         },
                         SessionCreationReturnCodes::InvalidRequestParameters => {
                             error!("InvalidRequestParameters: {}", response.message);
@@ -105,27 +110,15 @@ fn main() {
 
 }
 
-fn setup_logging(settings: &Settings) -> Result<(), fern::InitError> {
-    let logging_config = &settings.logging;
+fn setup_logging(verbose: bool) -> Result<(), fern::InitError> {
+    let logging_level = if verbose { log::LevelFilter::Debug } else { log::LevelFilter::Info };
 
-    let format_string = logging_config.format.clone();
-    let mut base_config = fern::Dispatch::new()
-        .format(move |out, message, record| {
-            let format = format_string
-                .as_deref()
-                .unwrap_or("[{timestamp}][{level}] {message}");
-            let formatted_message = format
-                .replace("{timestamp}", &chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.3f").to_string())
-                .replace("{level}", &record.level().to_string())
-                .replace("{message}", &message.to_string());
-            out.finish(format_args!("{}", formatted_message))
+    let base_config = fern::Dispatch::new()
+        .format(move |out, message, _| {
+            out.finish(format_args!("{}", &message.to_string()))
         })
-        .level(logging_config.level.parse::<log::LevelFilter>().unwrap_or(log::LevelFilter::Info));
-
-    // Enable console logging if configured.
-    if logging_config.console.unwrap_or(true) {
-        base_config = base_config.chain(std::io::stdout());
-    }
+        .level(logging_level)
+        .chain(std::io::stdout());
 
     // Apply the logging configuration.
     base_config.apply()?;
